2b9571ff58a00b689752b928daf9bdd2f3efcaee
zodiepupper <zodie@pupper.dev>
2026-07-05 17:04:44 -0400

    make nav bar responsive

--- js/header.js
+++ js/header.js
@@ -1,14 +1,16 @@
 document.write(`
   <header class="w-full z-100">
     <div class="navbar bg-base-100 shadow-lg">
-      <div class="flex flex-1">
+      <div class="flex flex-col md:flex-row flex-1">
         <a class="btn btn-primary text-xl md:2xl" href="index.html" title="go to home page">TimTrapsCats</a>
         <div class="inline border-primary border-2 rounded-2xl"></div>
         <a class="btn btn-ghost text-xl md:2xl" href="stopparvo.html" title="go to the Stop Parvo Springfield Mo program page">Stop Parvo Springfield MO</a>
         <div class="inline border-primary border-2 rounded-2xl"></div>
         <a class="btn btn-ghost text-xl md:2xl" href="ozarkstnr.html" title="go to the Ozarks TNR program page">Ozarks TNR</a>
+        <div class="inline border-primary border-2 rounded-2xl"></div>
+        <a class="btn btn-ghost text-xl md:2xl md:hidden" href="donate.html" title="go to the donate page">Donate!</a>
       </div>
-      <div>
+      <div class="hidden md:block">
         <ul class="menu menu-horizontal px-1">
           <li><a class="btn text-xl" href="donate.html" title="go to the donate page">Donate!</a></li>
 
